a3185e603be6428a65f5eaaea34d0b1463770b23
ReyBos <andreybosiy@yandex.ru>
2020-09-07 09:17:20 +0300

    5.4. Протеиновая диета, поправлено для варианта с одинаковой тягой [#352663]

--- src/ru/job4j/loop/Fitness.java
+++ src/ru/job4j/loop/Fitness.java
@@ -2,7 +2,7 @@ package ru.job4j.loop;
 
 public class Fitness {
     public static int calc(int ivan, int nik) {
-        int month = 0;
+        int month = ivan == nik ? 1 : 0;
         while (ivan < nik) {
             ivan *= 3;
             nik *= 2;
