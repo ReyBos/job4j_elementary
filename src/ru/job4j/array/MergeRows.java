4f2ed75af9ab731b8d749c0f224e78f545263963
reybos <andreybosiy@yandex.ru>
2020-10-18 13:03:34 +0300

    задания из раздела 1.1.5. Массивы

---
+++ src/ru/job4j/array/MergeRows.java
@@ -0,0 +1,17 @@
+package ru.job4j.array;
+
+public class MergeRows {
+    public static int[] merge(int[][] data) {
+        int length = 0;
+        for (int i = 0; i < data.length; i++) {
+            length += data[i].length;
+        }
+        int[] rsl = new int[length];
+        for (int i = 0, count = 0; i < data.length; i++) {
+            for (int j = 0; j < data[i].length; j++) {
+                rsl[count++] = data[i][j];
+            }
+        }
+        return rsl;
+    }
+}
---
+++ src/ru/job4j/array/RightDiagonal.java
@@ -0,0 +1,11 @@
+package ru.job4j.array;
+
+public class RightDiagonal {
+    public static int[] diagonal(int[][] data) {
+        int[] rsl = new int[data.length];
+        for (int i = 0, j = data.length - 1; i < data.length; i++, j--) {
+            rsl[i] = data[i][j];
+        }
+        return rsl;
+    }
+}
