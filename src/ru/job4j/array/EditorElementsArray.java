0897b5571d0d80a63ca12aaa8494db51c79e448e
reybos <andreybosiy@yandex.ru>
2020-10-21 22:07:21 +0300

    задания из раздела 1.1.5. Массивы

---
+++ src/ru/job4j/array/EditorElementsArray.java
@@ -0,0 +1,16 @@
+package ru.job4j.array;
+
+public class EditorElementsArray {
+    public static int[][] changeData(int[][] array, int el) {
+        for (int row = 0; row < array.length; row++) {
+            for (int cell = 0; cell < array[row].length; cell++) {
+                if (array[row][cell] > el) {
+                    array[row][cell] -= el;
+                } else {
+                    array[row][cell] = 0;
+                }
+            }
+        }
+        return array;
+    }
+}
