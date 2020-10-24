33e8ce4bf882b7717ce3247e3b0dd8fed1931f7e
reybos <andreybosiy@yandex.ru>
2020-10-24 08:02:45 +0300

    задания из раздела 1.1.5. Массивы

---
+++ src/ru/job4j/array/AndArray.java
@@ -0,0 +1,22 @@
+package ru.job4j.array;
+
+import java.util.Arrays;
+
+public class AndArray {
+    public static int[] and(int[] left, int[] right) {
+        int[] min = left.length >= right.length ? right : left;
+        int[] max = left.length >= right.length ? left : right;
+        int[] rsl = new int[min.length];
+        int size = 0;
+        for (int i = 0; i < min.length; i++) {
+            for (int j = 0; j < max.length; j++) {
+                if (min[i] == max[j]) {
+                    rsl[size] = min[i];
+                    size++;
+                    break;
+                }
+            }
+        }
+        return Arrays.copyOf(rsl, size);
+    }
+}
---
+++ src/ru/job4j/array/RandomDream.java
@@ -0,0 +1,8 @@
+package ru.job4j.array;
+
+public class RandomDream {
+    public static String random(String[] prizes, int num) {
+        int index = num % prizes.length - 1;
+        return prizes[index >= 0 ? index : prizes.length - 1];
+    }
+}
