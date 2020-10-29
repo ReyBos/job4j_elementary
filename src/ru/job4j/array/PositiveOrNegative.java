22f6edb1a2d13feaf3224280b0371a18b009b1e9
reybos <andreybosiy@yandex.ru>
2020-10-29 19:03:28 +0300

    задания из раздела 1.1.5. Массивы

---
+++ src/ru/job4j/array/PositiveOrNegative.java
@@ -0,0 +1,19 @@
+package ru.job4j.array;
+
+public class PositiveOrNegative {
+    public static boolean check(int[] data) {
+        int countNegative = 0;
+        int countPositive = 0;
+        for (int i = 0; i < data.length; i++) {
+            if (data[i] > 0) {
+                countPositive++;
+            } else {
+                countNegative++;
+            }
+        }
+        if (countNegative % 2 != 0) {
+            return true;
+        }
+        return false;
+    }
+}
